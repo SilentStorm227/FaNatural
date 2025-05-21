b0117c0ed4f1bc826285ad6a38423302804ca2c3
SilentStorm227 <stormpetal227@gmail.com>
2025-05-21 17:34:17 +0100

    adding html

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./components/home/Home";
 
+function App(){
+    return(
+        <div>
+            <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
 
-export default App
+            </Router>
+        </div>
+    )
+}
+
+export default App;
