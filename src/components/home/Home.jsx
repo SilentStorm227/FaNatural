49f911afab0b786530b02dd95d9b9a7be3eece6e
SilentStorm227 <stormpetal227@gmail.com>
2025-05-21 17:46:03 +0100

    done with essentials for now (Home)

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import "../style/home.css"
 
 function Home(){
     return(
         <div>
+            <div className="toptext">
             <h1>FaNaturals</h1>
+            <h5>Welcome to my online store</h5>
+            </div>
         </div>
     )
 }
