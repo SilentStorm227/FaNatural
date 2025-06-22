71981f0c1ebadc8b0eb6d83aa5f7c8177192f522
SilentStorm227 <stormpetal227@gmail.com>
2025-06-22 17:21:09 +0100

    Ilovecoding200

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -13,11 +13,44 @@ function Home(){
             <h1>FaNaturals</h1>
             </div>
             
-            <img src={img1}  className="image1"/>
+            <div className="my-container">
+                <div className="row">
+                    <div className="image-block">
+                        <img src={img1}  className="image1"/>
+                        <p>Leave-in Conditioner</p>
+                    </div>
+                </div>
+
+
+                <div className="row">
+                    <div className="image-block">
             <img src={img2}  className="image1"/>
+            <p>Herbal Bloom Cleansing Shampoo</p>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="image-block">
             <img src={img3}  className="image1"/>
+            <p>Deep and Intensive Hair Conditioner</p>
+                    </div>
+            </div>
+
+                <div className="row">
+                    <div className="image-block">
             <img src={img4}  className="image1"/>
+            <p>GrowThrive Oil</p>
+                    </div>
+            </div>
+
+                <div className="row">
+                    <div className="image-block">
             <img src={img5}  className="image1"/>
+            <p>Pre-wash Hair Treatment Oil</p>
+                    </div>
+            </div>
+            </div>
+
         </div>
     )
 }
