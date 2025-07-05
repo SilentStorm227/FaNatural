be75f1eb2044e01a8dbb2a4a3f708c87a43f0a8e
SilentStorm227 <stormpetal227@gmail.com>
2025-07-05 13:58:44 +0100

    navbar looks good

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes, Route, Link } from 'react-router-dom';
+import "../style/navbar.css";
 import Home from "../home/Home";
+import cart from "../images/cart.svg";
 
 function NavBar(){
     return(
@@ -8,6 +10,9 @@ function NavBar(){
             <nav className="nav">
                 <div className="header">
                     <Link to='/'>Hair</Link>
+                    <Link to='/Signup' className="signup">Sign up</Link>
+                    <Link to='/Login' className="login">Login</Link>
+                    <Link to='/cart'><img src={cart} className="cart"/></Link>
                 </div>
             </nav>
         </div>
