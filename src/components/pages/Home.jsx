c5c768b5d40f82510b41ab9b52c0124413d214de
SilentStorm227 <stormpetal227@gmail.com>
2025-07-12 13:47:09 +0100

    willcontinue latter

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import pic1 from "../images/pic1.jpeg"
 import pic2 from "../images/pic2.jpeg"
 import "../style/home.css"
@@ -9,10 +9,11 @@ function Home(){
             <div className="toptext">
             <h1>FaNaturals</h1>
             </div>
-            <div className="container-fluid mt5">
-                <div className="card-body">
+            {/* <div className="container-fluid mt5">
+                <div className="card-body"> */}
             <img src={pic1} className="card"/>
             <img src={pic2} className="card"/>
+            {/* <img src={pic2} className="card"/>
             <img src={pic2} className="card"/>
             <img src={pic2} className="card"/>
             <img src={pic2} className="card"/>
@@ -21,10 +22,9 @@ function Home(){
             <img src={pic2} className="card"/>
             <img src={pic2} className="card"/>
             <img src={pic2} className="card"/>
-            <img src={pic2} className="card"/>
-            <img src={pic2} className="card"/>
-                </div>
-            </div>
+            <img src={pic2} className="card"/> */}
+                {/* </div>
+            </div> */}
         <p className="description">Welcome to FNaturals — a space dedicated to healthy hair, protective styling, and intentional care.<br /> <br />
 We provide mobile and home-based braiding services, detailed wig care, scalp and hair treatments, and handcrafted products made to nourish, strengthen, and support natural hair.<br /> Whether you're getting styled, treating your scalp, or shopping for haircare essentials, FNaturals brings quality, comfort, and creativity right to you. <br />Every service is done with clean techniques, professional care, and a passion for helping you feel confident in every strand.</p>
         </div>
