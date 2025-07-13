bfb78d8a705b0f70e741392566cc3b98e4b5fcd3
SilentStorm227 <stormpetal227@gmail.com>
2025-07-13 14:56:25 +0100

    started slideshow

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -8,23 +8,8 @@ function Home(){
         <div>
             <div className="toptext">
             <h1>FaNaturals</h1>
-            </div>
-            {/* <div className="container-fluid mt5">
-                <div className="card-body"> */}
-            <img src={pic1} className="card"/>
-            <img src={pic2} className="card"/>
-            {/* <img src={pic2} className="card"/>
-            <img src={pic2} className="card"/>
-            <img src={pic2} className="card"/>
-            <img src={pic2} className="card"/>
-            <img src={pic2} className="card"/>
-            <img src={pic2} className="card"/>
-            <img src={pic2} className="card"/>
-            <img src={pic2} className="card"/>
-            <img src={pic2} className="card"/>
-            <img src={pic2} className="card"/> */}
-                {/* </div>
-            </div> */}
+            </div>            
+        <Slideshow />
         <p className="description">Welcome to FNaturals — a space dedicated to healthy hair, protective styling, and intentional care.<br /> <br />
 We provide mobile and home-based braiding services, detailed wig care, scalp and hair treatments, and handcrafted products made to nourish, strengthen, and support natural hair.<br /> Whether you're getting styled, treating your scalp, or shopping for haircare essentials, FNaturals brings quality, comfort, and creativity right to you. <br />Every service is done with clean techniques, professional care, and a passion for helping you feel confident in every strand.</p>
         </div>
