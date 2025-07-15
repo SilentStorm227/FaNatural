29272947bd6b853c2eb134a6cac4470cc930c9b3
SilentStorm227 <stormpetal227@gmail.com>
2025-07-15 20:16:32 +0100

    going

---
+++ src/components/pages/Appointment.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Appointment(){
+    return(
+        <div>
+            <h1 className="text">Ready for your next hair transformation? Click the link below to schedule <br />an appointment and let’s make hair magic happen.</h1>
+        </div>
+    )
+};
+
+export default Appointment;
