8dd0fed889b5e5c784aa344953329d67ef0f3422
SilentStorm227 <stormpetal227@gmail.com>
2025-07-30 19:51:37 +0100

    serivices

---
+++ src/components/pages/Treatment.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Treatment(){
+    return(
+        <div>
+            <div className="top">
+                <h1>Braids</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Treatment;
---
+++ src/components/pages/Wigcare.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Wigcare(){
+    return(
+        <div>
+            <div className="top">
+                <h1>Braids</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Wigcare;
