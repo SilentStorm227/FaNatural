f9357e80e6c071ab33a7f8f324e18a4770b27e79
SilentStorm227 <stormpetal227@gmail.com>
2025-08-01 23:29:23 +0100

    adjustments

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -8,14 +8,34 @@ function NavBar(){
         <div>
             <nav className="nav">
                 <div className="header">
-                    <Link to='/' className="left">Hair</Link>
-                    <Link to='/about' className="left">About</Link>
-                    <Link to='/services' className="left">Services</Link>
-                    <Link to='/appointments' className="left">Appointments</Link>
-                    <Link to='/store' className="left">Store</Link>
+                    <Link to='/' className="left">
+                    <button className="b1">Hair</button>
+                        </Link>
+
+                    <Link to='/about' className="left">
+                    <button className="b1">About</button>
+                    </Link>
                     
-                    <Link to='/Signup' className="right">Sign up</Link>
-                    <Link to='/Login' className="right">Login</Link>
+                    <Link to='/services' className="left">
+                    <button className="b1">Services</button>
+                    </Link>
+
+                    <Link to='/appointments' className="left">
+                    <button className="b1">Appointments</button>
+                    </Link>
+
+                    <Link to='/store' className="left">
+                    <button className="b1">Store</button>
+                    </Link>
+                    
+                    <Link to='/Signup' className="right">
+                    <button className="b1">Sign up</button>
+                    </Link>
+
+                    <Link to='/Login' className="right">
+                    <button className="b1">Login</button>
+                    </Link>
+
                     <Link to='/cart'><img src={cart} className="cart"/></Link>
                 </div>
             </nav>
