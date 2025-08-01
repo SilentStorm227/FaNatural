6028543fbc7fe9374ed039459fa74ba9425945bf
SilentStorm227 <stormpetal227@gmail.com>
2025-08-01 22:43:31 +0100

    product page

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import "../style/products.css"
 
-function Products(){
+function Wigcare(){
     return(
         <div>
             <div className="top">
-                <h1>Braids</h1>
+                <h1>Haircare Products</h1>
+                <p>Handcrafted with care to nourish, protect, and grow your natural hair.</p>
             </div>
+            <div className="service">
+                <h2>Available Products:Pre-poo treatments </h2>
+                <ul>
+                <li>Shampoo</li>
+                <li>* Rinse-Off Conditioner</li>
+                <li>Leave-In Conditioner</li>
+                <li>Hair Serum</li>
+                <li>Ayurvedic Hair Growth Oil</li>
+                </ul>
+            </div>
+            <h1>Images Available at store</h1>
+            <button>Store</button>
         </div>
     )
 }
 
-export default Products;
+export default Wigcare;
