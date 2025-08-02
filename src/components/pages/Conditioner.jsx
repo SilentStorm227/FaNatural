37de1e691c0e188b7f8c49fa2364d3ba72788d14
SilentStorm227 <stormpetal227@gmail.com>
2025-08-02 23:22:21 +0100

    adjustment

--- src/components/pages/Conditioner.jsx
+++ src/components/pages/Conditioner.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import "../style/conditioner.css"
 import I1 from "../images/1.jpeg"
 
 function Conditioner(){
     return(
         <div>
-
+            <img src={I1} />
         </div>
     )
 }
+
+export default Conditioner;
