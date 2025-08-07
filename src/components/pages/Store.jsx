bcab5896bc9ada07c8768bf3441d6399cc5eba16
SilentStorm227 <stormpetal227@gmail.com>
2025-08-07 11:51:55 +0100

    store

--- src/components/pages/Store.jsx
+++ src/components/pages/Store.jsx
@@ -16,36 +16,24 @@ function Store(){
             <div className="container-fluid mt-5">
                 <div className="card-body">
 
-
-
-
                         <Link to="/Leave-in-conditioner">
                         <img src={img1}  className="image1"/>
                         </Link>
-                        <span>
-                        <p className="p1">Leave-in Conditioner</p>
-                        </span>
 
             <img src={img2}  className="image1"/>
-                    <span>
-            <p className="p1"> Herbal Bloom Cleansing Shampoo</p>
-                    </span>
+
             <img src={img3}  className="image2"/>
 
-            <span>
-            <p className="p2">Deep and Intensive Hair Conditioner</p>
-            </span>
-
             <img src={img4}  className="image2"/>
-            <span>
-            <p className="p2">GrowThrive Oil</p>
-            </span>
 
             <img src={img5}  className="image3"/>
-            <span>
-            <p className="p3">Pre-wash Hair Treatment Oil</p>
-            </span>
 
+
+                        <p className="p1">Leave-in Conditioner</p>
+            <p className="p1"> Herbal Bloom Cleansing Shampoo</p>
+            <p className="p2">Deep and Intensive Hair Conditioner</p>
+            <p className="p2">GrowThrive Oil</p>
+            <p className="p3">Pre-wash Hair Treatment Oil</p>
 
                 </div>
             </div>
