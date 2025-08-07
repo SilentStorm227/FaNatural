702469fe3ba390ad18c0951129c65dc1319230b6
SilentStorm227 <stormpetal227@gmail.com>
2025-08-07 11:41:31 +0100

    store

--- src/components/pages/Store.jsx
+++ src/components/pages/Store.jsx
@@ -13,55 +13,55 @@ function Store(){
             <h1>FaNaturals</h1>
             </div>
             
-            <div className="my-container">
+            {/* <div className="my-container">
                 <div className="row">
-                    <div className="image-block">
+                    <div className="image-block"> */}
                         <Link to="/Leave-in-conditioner">
                         <img src={img1}  className="image1"/>
                         </Link>
                         <span>
                         <p className="p1">Leave-in Conditioner</p>
                         </span>
-                    </div>
-                </div>
+                    {/* </div> */}
+                {/* // </div> */}
 
 
-                <div className="row">
-                    <div className="image-block">
+                {/* <div className="row">
+                    <div className="image-block"> */}
             <img src={img2}  className="image1"/>
                     <span>
             <p className="p1"> Herbal Bloom Cleansing Shampoo</p>
                     </span>
-                    </div>
-                </div>
+                    {/* </div>
+                </div> */}
 
-                <div className="row">
-                    <div className="image-block">
+                {/* <div className="row">
+                    <div className="image-block"> */}
             <img src={img3}  className="image2"/>
             <span>
             <p className="p2">Deep and Intensive Hair Conditioner</p>
             </span>
-                    </div>
-            </div>
+                    {/* </div>
+            </div> */}
 
                 <div className="row">
-                    <div className="image-block">
+                    {/* <div className="image-block"> */}
             <img src={img4}  className="image2"/>
             <span>
             <p className="p2">GrowThrive Oil</p>
             </span>
-                    </div>
-            </div>
+                    {/* </div>
+            </div> */}
 
-                <div className="row">
-                    <div className="image-block">
+                {/* <div className="row">
+                    <div className="image-block"> */}
             <img src={img5}  className="image3"/>
             <span>
             <p className="p3">Pre-wash Hair Treatment Oil</p>
             </span>
-                    </div>
-            </div>
-            </div>
+                    {/* </div>
+            </div> */}
+            // </div>
 
         </div>
     )
