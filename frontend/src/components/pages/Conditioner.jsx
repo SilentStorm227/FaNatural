f6be2c321a9daa47d7bc98e394217fdd4cc230e2
SilentStorm227 <stormpetal227@gmail.com>
2025-08-17 15:44:32 +0100

    add to cart button

--- frontend/src/components/pages/Conditioner.jsx
+++ frontend/src/components/pages/Conditioner.jsx
@@ -5,16 +5,19 @@ import minus from "../images/minus.svg";
 import plus from "../images/plus.svg"
 
 function Conditioner(){
+
+    const add = ()=>{
+        alert('added')
+    }
+
     return(
         <div>
             <div className="textt1">
                 <h1>Leave in conditiner.</h1>
             </div>
+            <button className="cart1" onClick={add} >Add to cart</button>        
 
             <p><img className="img" src={I1} /> Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br />Aliquid rerum aspernatur quis mollitia quasi eaque expedita, <br />quam at sequi aliquam consectetur, voluptatem impedit sapiente ex <br />labore. Dolorem reiciendis saepe consequuntur? </p>
-             <button className="cart1">
-                Add to cart
-                </button>
 
                 <img src={minus} className="decrease" />
 
