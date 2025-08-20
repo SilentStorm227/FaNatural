ff3db553ded110f1666113d68b863372984ce71d
SilentStorm227 <stormpetal227@gmail.com>
2025-08-20 12:14:16 +0100

    store

--- frontend/src/components/pages/Conditioner.jsx
+++ frontend/src/components/pages/Conditioner.jsx
@@ -1,45 +1,31 @@
 import React from "react";
-import "../style/conditioner.css";
+import "../style/conditioner.css"
 import I1 from "../images/1.jpeg";
 import minus from "../images/minus.svg";
 import plus from "../images/plus.svg"
 
-function Conditioner(){
-
+function conditioner(){
     const add = ()=>{
         alert('added')
-    }
+    };
 
     return(
         <div>
-            {/* <div className="textt1">
-                <h1>Leave in conditiner.</h1>
+            <div className="textt1">
+                <h1>Cleansing Shampoo</h1>
             </div>
-            <button className="cart1" onClick={add} >Add to cart</button>        
 
             <p><img className="img" src={I1} /> Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br />Aliquid rerum aspernatur quis mollitia quasi eaque expedita, <br />quam at sequi aliquam consectetur, voluptatem impedit sapiente ex <br />labore. Dolorem reiciendis saepe consequuntur? </p>
+            <button className="cart2" onClick={add}>Add to cart</button>
+            
+                <img src={minus} className="decrease2" />
 
-                <img src={minus} className="decrease" />
+                <input className="number2" defaultValue={0}/>
 
-                <input className="number" defaultValue={0}/>
-
-                <img src={plus} className="increase" /> */}
-
-                <div className="">
-                    <h1>Leave in conditioner</h1>
-                </div>
-                <button className="">Add to cart</button>
-                <p>
-                    <img className="" src={I1} />
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, <br />veritatis beatae fugit dignissimos non delectus minus voluptatibus autem?<br /> reprehenderit temporibus porro quae consectetur in. Dolore aliquid ullam quo saepe ipsa.
-                </p>
-                <img src={minus} className="decrease" />
-                <input className="number" defaultValue={0} />
-                <img src={plus} className="increase" />
-
+                <img src={plus} className="increase2" />
 
         </div>
     )
 }
 
-export default Conditioner;
+export default conditioner;
