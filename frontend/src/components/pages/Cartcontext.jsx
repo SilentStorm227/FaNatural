5f8f65cdddf86e2946cfa54325f3c32f1fa599c5
SilentStorm227 <stormpetal227@gmail.com>
2025-08-22 15:32:38 +0100

    add to staore

--- frontend/src/components/pages/Cartcontext.jsx
+++ frontend/src/components/pages/Cartcontext.jsx
@@ -1,23 +1,30 @@
 import { useContext, createContext, useState } from "react";
 
+// Create the Cart context (like a global storage for cart state)
 const Cartcontext = createContext();
 
+// The provider wraps around your app so all components can use the cart
 export function CartProvider({children}){
+      // State: keeps track of all items in the cart
     const [cart, setCart] = useState([]);
 
+      // Function: add a product to the cart
     const addtocat = (product) =>{
         setCart((prev) =>{
+                  // Check if product is already in cart
             const existing = prev.find((item) => item.id === product.id);
-
             if (existing){
+                        // If yes, increase its quantity
                 return prev.map((item) =>
                 item.id === product.id ? {...item, qty: item.qty + 1} : item
                 );
             }
+                  // If not, add new product with qty = 1
             return [...prev, { ...product, qty:1 }];
         });
     };
 
+      // Function: increase product quantity
     const increaseQty = (id) => {
         setCart((prev)=>
         prev.map((item>
@@ -27,9 +34,11 @@ export function CartProvider({children}){
         );
     };
 
+      // Function: decrease product quantity
     const decreaseQty = (id) => {
         setCart((prev)=>
         prev.map((item)=>
+                    // Only decrease if qty is more than 1
         item.id === id && item.qty > 1
         ? {...item, qty: item.qty - 1}
         :item
@@ -37,6 +46,7 @@ export function CartProvider({children}){
     );
     };
 
+      // Function: remove product completely
      const removefromCart = (id) => {
         setCart((prev)=> prev.filter((item) => item.id !== id));
      };
