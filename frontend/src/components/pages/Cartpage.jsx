4c0e61c94925407bb3258f8317ed8a6a9325c2d3
SilentStorm227 <stormpetal227@gmail.com>
2025-08-23 19:22:02 +0100

    button

--- frontend/src/components/pages/Cartpage.jsx
+++ frontend/src/components/pages/Cartpage.jsx
@@ -4,17 +4,25 @@ import {useCart} from "./Cartcontext";
 
 function Cartpage(props){
     const {cart} = useCart();
+    // console.log("Cart state:", cart);
+    console.log(cart);
     return(
         <div>
             <div>
                 <h1>Welcome to your cart.</h1>
+
                 {cart.length === 0 && <p>cart is empty</p>}
-                {cart.map((item)=>{
+
+                {cart.map((item)=>(
                     <div key={item.id}>
-                        <p>{item.name} * {item.qty} - £{item.price}</p>
+                        <p>{item.name} * {item.qty} - £{item.price * item.qty}</p>
                     </div>
-                })}
+                ))}
             </div>
+
+            {cart.length > 0 && (
+                <p><Strong>Total:£{cart.reduce((sum, item) => sum + item.price * item.qty, 0)}</Strong></p>
+            )}
         </div>
     )
 };
