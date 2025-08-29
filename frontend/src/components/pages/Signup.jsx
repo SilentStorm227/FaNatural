7491aa05759e7f43eb29da3f975525abc8b89176
SilentStorm227 <stormpetal227@gmail.com>
2025-08-29 13:50:32 +0100

    signup page

--- frontend/src/components/pages/Signup.jsx
+++ frontend/src/components/pages/Signup.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
 import "../style/signup.css"
 
-function Signup(){
+const Signup = ()=>{
+
+    const [formdata, setformdata] = useState({
+        Name:'',
+        Email:'',
+        Password:''
+    });
+
+    const handlechange = (e)=>{
+        const {name, value} = e.target
+        setformdata({
+            ...FormData,
+            [name]: value,
+        });
+    };
+
+    const handlesubmit = async (e)=>{
+        e.preventdefault();
+
+        try {
+            const response = await axios.post('http://localhost:5000/create-account', FormData);
+            alert('Account created sucessfully!!')
+        } catch (error) {
+            if (error.response) {
+                console.error('error creating account:', error.response.data);
+            }
+            else{
+                console.error('error creating account:', error.message);
+            }
+            alert('error creating account');
+        }
+    }
+
     return(
         <div>
             <div className="toptext">
@@ -9,9 +42,11 @@ function Signup(){
             </div>
             
             <div className="content">
-                <input type="text" placeholder="Name"/><br /><br />
-                <input type="password" placeholder="Password"/><br /><br />
-                <input type="email" placeholder="Email"/><br /><br />
+                <form onSubmit={handlesubmit}>
+                <input type="text" placeholder="Name" value={formdata.Name} onChange={handlechange}/><br /><br />
+                <input type="email" placeholder="Email" value={formdata.Email} onChange={handlechange} /><br /><br />
+                <input type="password" placeholder="Password" value={formdata.Password} onChange={handlechange} /><br /><br />
+                </form>
             </div>
             <div className="end">
                 <button>Sign up</button>
