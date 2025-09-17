afceb89cc946507ec68138254321389378bf5c3e
SilentStorm227 <stormpetal227@gmail.com>
2025-09-17 18:18:30 +0100

    profile pic

--- frontend/src/components/pages/Profile.jsx
+++ frontend/src/components/pages/Profile.jsx
@@ -4,7 +4,36 @@ import "../style/profile.css"
 
 const Profile = ()=>{
     const [profile, setprofile] = useState(null);
-        const [error, setError] = useState(null);
+    const [error, setError] = useState(null);
+
+    const profileupload = () => {
+        const [file, setFile] = useState(null);
+
+        const handlechange = (e) =>{
+            setFile(e.target.file[0]);
+        };
+
+        const handleupload = async ()=>{
+            if (!file) return alert('Select a file first!!');
+            const formData = new formData();
+            formData.append('profilePic', file);
+
+            try {
+                const token = localStorage.getItem('token');
+                const res = await axios.post('http://localhost:5000/upload-profile', formData, {
+                    header:{
+                        'Authorization': `Bearer ${token}`,
+          'Content-Type': 'multipart/form-data',
+                    },
+                });
+                alert('uploaded succesfully!!');
+                      console.log(res.data);
+            } catch (error) {
+                console.error(err);
+      alert('Error uploading');
+            }
+        };
+    }
 
         useEffect(()=>{
             const fetchprofile = async ()=>{
@@ -45,6 +74,14 @@ const Profile = ()=>{
         
     return(
         <div>
+            {profile.profilePic &&(
+                <img src={`http://localhost:5000/${profile.profilePic}`}
+                lt="Profile"
+        style={{ width: '150px', height: '150px', borderRadius: '50%' }}
+        />
+            )}
+            <input type='file' accept='image/*' onChange={handlechange} />
+            <button onClick={handleupload}>Upload picture</button>
             <h1 className='ttex1t'>Profile</h1><br /><br />
             <p className='thetext' ><strong>Name:</strong> {profile.Name}</p>
             <p className='thetext'><strong>Email:</strong> {profile.Email}</p>
